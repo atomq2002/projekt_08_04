21817f517cbc991d19ea9e8fbf65ed3f79906690
t <t>
2025-04-08 14:32:31 +0200

    initial commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace projekt
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] tablica= new int[100];
+            for(int i=0;i<100;i++)
+            {
+                tablica[i]=i;
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
